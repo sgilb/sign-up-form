9c12da2f351589372640779911bf4427030ca592
sgilb <32495429+sgilb@users.noreply.github.com>
2024-01-20 23:43:55 +0000

    Add submission logic and password error handling

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("signup");
+const passwordError = document.getElementById("password-error");
+
+form.addEventListener("submit", event => {
+  if (document.getElementById("password").value !== document.getElementById("password-confirm").value) {
+    event.preventDefault();
+    passwordError.style.visibility = "visible";
+  } else {
+    passwordError.style.visibility = "hidden";
+    alert("Form submitted ✅");
+  }
+});
